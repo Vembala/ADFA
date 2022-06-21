44dfc2ea15d84d85cbcc7a990f50129200e068e6
Sreerag M <sreeragibtl@gmail.com>
2022-06-21 18:46:19 +0530

    Test the interface.

---
+++ test/test_image_input.py
@@ -0,0 +1,31 @@
+"""The test case for image input
+
+This module contain tests for the image interface.
+
+Example:
+    $ python -m pytest test_image_input.py
+
+Attributes:
+"""
+
+import gradio
+
+import pytest
+from ADFA.gradio_interface import GradioInterface
+
+ID = 0
+
+def test_image_interface_exist(id: int=ID):
+
+    """This function test if image interface instantiated
+
+    This function test if the gradio app contains the image
+    input.
+
+    Args:
+        id (int): The ID of the component.
+
+    Returns
+    """
+
+    assert type(GradioInterface.input_components[ID]) == gradio.Image
