60eb0a82ceace9f218c6d32d2b550d143b73e9eb
Sreerag M <sreeragibtl@gmail.com>
2022-06-23 18:43:55 +0530

    Update function.

--- test/test_result.py
+++ test/test_result.py
@@ -7,14 +7,14 @@ AUDIO_PATH = "test/example_audio.wav"
 WRITE_PATH = "test/result.mp4"
 
 
-def remove_(path: str):
-    pass
+def remove_(path: str) -> None:
+    return None
 
 
 choices = {True: os.remove, False: remove_}
 remove = choices[os.path.exists(WRITE_PATH)]
 
-remove(WRITE_PATH)
+_ = remove(WRITE_PATH)
 
 
 def test_predict(image_path: str = IMAGE_PATH, audio_path: str = AUDIO_PATH):
