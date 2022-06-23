6f078ded09829af36d51b1ea8cff1bbc779fbc38
Sreerag M <sreeragibtl@gmail.com>
2022-06-23 15:48:22 +0530

    Make sure file exist.

--- test/test_result.py
+++ test/test_result.py
@@ -5,7 +5,13 @@ IMAGE_PATH = "test/image.jpeg"
 AUDIO_PATH = "test/example_audio.wav"
 WRITE_PATH = "test/result.mp4"
 
-os.remove(WRITE_PATH)
+def remove_(path: str):
+    pass
+
+choices = {True: os.remove, False: remove_}
+remove = choices[os.path.exists(WRITE_PATH)]
+
+remove(WRITE_PATH)
 
 def test_predict(image_path: str, audio_path: str):
     ret_path = predict(image_path=image_path, audio_path=audio_path)
