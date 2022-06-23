63d42aa3044ef431c7e67b81c338990e91a1614a
Sreerag M <sreeragibtl@gmail.com>
2022-06-23 18:16:18 +0530

    Update path.

--- ADFA/gradio_interface.py
+++ ADFA/gradio_interface.py
@@ -28,7 +28,7 @@ animator = sda.VideoAnimator(model_path=MODEL_PATH)
 
 def predict(image_path: str, audio_path: str, animator = animator, path: str = OUTPUT_PATH):
     vid, aud = animator(image_path, audio_path)
-    animator.save_video(vid, aud, OUTPUT_PATH)
+    animator.save_video(vid, aud, path)
 
 inputs = [
     gradio.Image(type="filepath"),
