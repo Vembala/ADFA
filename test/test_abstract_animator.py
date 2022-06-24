2347201fdb3a36378d89b0beaa1c427307efee6b
Sreerag M <sreeragibtl@gmail.com>
2022-06-24 22:30:01 +0530

    Update type.

--- test/test_abstract_animator.py
+++ test/test_abstract_animator.py
@@ -5,5 +5,5 @@ import typing
 from ADFA.abstract import AnimatorAbstract
 
 
-def test_abstract_animator(abstract: typing.Type(AnimatorAbstract) = AnimatorAbstract) -> None:
+def test_abstract_animator(abstract: typing.Type[AnimatorAbstract] = AnimatorAbstract) -> None:
     assert inspect.isabstract(abstract)
