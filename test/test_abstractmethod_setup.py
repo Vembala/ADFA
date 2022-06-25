b42fcafafd68fe1cb7d2b6c13a15ff9628163425
Sreerag M <sreeragibtl@gmail.com>
2022-06-25 19:54:04 +0530

    add missing fixture

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -2,5 +2,5 @@ import inspect
 import typing
 from ADFA.abstract import AnimatorAbstract
 
-def test_setup_exist(abstract: typing.Type[AnimatorAbstract]):
+def test_setup_exist(abstract: typing.Type[AnimatorAbstract] = AnimatorAbstract):
     assert inspect.isabstract(AnimatorAbstract.setup)
