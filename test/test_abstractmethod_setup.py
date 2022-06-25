7915c0a5f31843967f66d9bb9c443233cb59091f
Sreerag M <sreeragibtl@gmail.com>
2022-06-25 20:22:24 +0530

    update test

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -1,6 +1,10 @@
 import inspect
 import typing
+import pytest
 from ADFA.abstract import AnimatorAbstract
 
-def test_setup_exist(abstract: typing.Type[AnimatorAbstract] = AnimatorAbstract):
-    assert inspect.isabstract(AnimatorAbstract.setup)
+PARAMETERS = [(AnimatorAbstract, ['setup'])]
+
+@pytest.fixtures.parameterize("abstract, name", PARAMETERS)
+def test_setup_exist(abstract: typing.Type[AnimatorAbstract], name: str):
+    assert sorted(NAMES) == sorted(list(AnimatorAbstract.__abstractmethods__))
