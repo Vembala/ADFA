57ea56ac40eac43c4afb5d44586c4141396d00a2
Sreerag M <sreeragibtl@gmail.com>
2022-06-25 19:49:35 +0530

    fix errpr in name

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -1,6 +1,6 @@
 import inspect
 import typing
-from ADFA.abstracts import AnimatorAbstract
+from ADFA.abstract import AnimatorAbstract
 
 def test_setup_exist(abstract: typing.TYPE[AnimatorAbstract]):
     assert inspect.isabstract(AnimatorAbstract.setup)
