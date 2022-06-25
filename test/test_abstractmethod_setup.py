05280fa593866cb8c9fa3f3c00344b870ea1a0a5
Sreerag M <sreeragibtl@gmail.com>
2022-06-25 20:24:37 +0530

    update test

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -6,5 +6,5 @@ from ADFA.abstract import AnimatorAbstract
 PARAMETERS = [(AnimatorAbstract, ['setup'])]
 
 @pytest.fixtures.parameterize("abstract, name", PARAMETERS)
-def test_setup_exist(abstract: typing.Type[AnimatorAbstract], name: str):
-    assert sorted(NAMES) == sorted(list(AnimatorAbstract.__abstractmethods__))
+def test_setup_exist(abstract: typing.Type[AnimatorAbstract], names: str):
+    assert sorted(names) == sorted(list(AnimatorAbstract.__abstractmethods__))
