b4f2a2f3bf7b0bd3754902ad560c27697404db7d
Sreerag M <sreeragibtl@gmail.com>
2022-06-25 19:51:49 +0530

    fix error in name Type

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -2,5 +2,5 @@ import inspect
 import typing
 from ADFA.abstract import AnimatorAbstract
 
-def test_setup_exist(abstract: typing.TYPE[AnimatorAbstract]):
+def test_setup_exist(abstract: typing.Type[AnimatorAbstract]):
     assert inspect.isabstract(AnimatorAbstract.setup)
