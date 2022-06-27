e8accdf812d66866f0bf16b9794580ebca8dc880
Sreerag M <sreeragibtl@gmail.com>
2022-06-27 19:31:18 +0530

    fix missing parantheses

--- test/test_arguments.py
+++ test/test_arguments.py
@@ -4,7 +4,7 @@ import pytest
 
 import ADFA
 
-PAIRS = [(ADFA.abstract.setup, ['argdict'],]
+PAIRS = [(ADFA.abstract.setup, ['argdict']),]
 
 @pytest.mark.parametrize("function, arguments", )
 def test_arguments(function: typing.Function, arguments: typing.List):
