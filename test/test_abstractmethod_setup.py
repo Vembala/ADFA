3fe723641146ba693228c1265ee83fe0dc0f2207
Sreerag M <sreeragibtl@gmail.com>
2022-06-27 21:55:49 +0530

    assert methods in MakeItTalk

--- test/test_abstractmethod_setup.py
+++ test/test_abstractmethod_setup.py
@@ -4,10 +4,11 @@ import typing
 import pytest
 
 from ADFA.abstract import AnimatorAbstract
+from ADFA.makeittalk import MakeItTalk
 
-PARAMETERS = [(AnimatorAbstract, ["setup", "predict"])]
+PARAMETERS = [(AnimatorAbstract, ["setup", "predict"]), (MakeItTalk, list(AnimatorAbstract.__abstractmethods__))]
 
 
 @pytest.mark.parametrize("abstract, names", PARAMETERS)
-def test_setup_exist(abstract: typing.Type[AnimatorAbstract], names: str):
-    assert sorted(names) == sorted(list(AnimatorAbstract.__abstractmethods__))
+def test_setup_exist(aclass: typing.Type[AnimatorAbstract], names: str):
+    assert sorted(names) == sorted(list(aclass.__abstractmethods__))
