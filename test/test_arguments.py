11ac5b2b3201a7a3dec23934c50089690a31cb8a
Sreerag M <sreeragibtl@gmail.com>
2022-06-27 20:03:57 +0530

    fix missing argument self

--- test/test_arguments.py
+++ test/test_arguments.py
@@ -4,7 +4,7 @@ import pytest
 
 import ADFA
 
-PAIRS = [(ADFA.abstract.AnimatorAbstract.setup, ['argdict']),]
+PAIRS = [(ADFA.abstract.AnimatorAbstract.setup, ["self", 'argdict']),]
 
 @pytest.mark.parametrize("function, arguments", PAIRS)
 def test_arguments(function: typing.Callable, arguments: typing.List):
